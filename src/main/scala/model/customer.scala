7ede2d532e658b3cf6a37cae07bafa7606a8c1a7
Randhir <randhir.kumar@knoldus.in>
2018-02-25 23:57:18 +0530

    DataFrame added

--- src/main/scala/model/customer.scala
+++ src/main/scala/model/customer.scala
@@ -2,4 +2,4 @@ package model
 
 case class customer(customer_id: Int, name: String, street: String, city: String, state: String, zip: Int)
 
-case class sales(year: Int, month: Int, day: Int, customer_id: Int, sales_price: Double)
+case class sales(year: Int, month: Int, day: Int, customer_id: Int, price: Double)
--- src/main/scala/view/Application.scala
+++ src/main/scala/view/Application.scala
@@ -3,6 +3,7 @@ package view
 import java.util.{Calendar, Date}
 
 import model.{customer, sales}
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Time {
@@ -33,11 +34,16 @@ object Application extends App {
 
   val conf = new SparkConf().setAppName("spark-assignment").setMaster("local[*]")
   val sc = new SparkContext(conf)
-  val customerData = sc.textFile("/home/knoldus/Downloads/SparkPractice/files/customer").map(x => x.split("#"))
-  val salesData = sc.textFile("/home/knoldus/Downloads/SparkPractice/files/sales").map(x => x.split("#"))
-  val customer_record = customerData.map(x => (x(0).toInt, customer(x(0).toInt, x(1), x(2), x(3), x(4), x(5).toInt)))
-  val sales_record = salesData.map(x => (x(1).toInt, sales(Time.getYear(x(0).toInt), Time.getMonth(x(0).toInt), Time.getDay(x(0).toInt), x(1).toInt, x(2).toDouble)))
-  val join_rdd = customer_record.join(sales_record)
-  join_rdd.foreach(println)
+  val sqlContext = new SQLContext(sc)
+
+  import sqlContext.implicits._
+
+  val customerData = sc.textFile("/home/knoldus/Downloads/spark-assignment-02/files/customer").map(x => x.split("#"))
+  val salesData = sc.textFile("/home/knoldus/Downloads/spark-assignment-02/files/sales").map(x => x.split("#"))
+  val customer_record = customerData.map(x => customer(x(0).toInt, x(1), x(2), x(3), x(4), x(5).toInt)).toDF()
+  val sales_record = salesData.map(x => sales(Time.getYear(x(0).toInt), Time.getMonth(x(0).toInt), Time.getDay(x(0).toInt), x(1).toInt, x(2).toDouble)).toDF()
+
+  val innerJoin = sales_record.join(customer_record, sales_record("customer_id") === customer_record("customer_id"), "inner")
+  innerJoin.select("state", "year", "price").show
   sc.stop()
 }
